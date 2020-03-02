226ac1cb73bb446212612551481daab0d1e16943
OmarQuezada13 <omar.wst13@gmail.com>
2020-03-02 10:12:53 -0600

    Commit Modulo academia Terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AcademiaComponent } from './academia/academia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcademiaComponent
   ],
   imports: [
     BrowserModule,
