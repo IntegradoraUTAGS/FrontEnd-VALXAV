c13f5f8bfbe5705abf971e38ce62780ac935b1d9
AdanJarami <est2615adan@gmail.com>
2020-03-03 17:27:51 -0600

    PERIODOS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AltacarreraComponent } from './altacarrera/altacarrera.component';
+import { AltaperiodoComponent } from './altaperiodo/altaperiodo.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
@@ -11,6 +12,7 @@ const routes: Routes = [
   {path:'login', component:LoginComponent},
   {path:'register', component:RegisterComponent},
   {path:'altacarrera', component:AltacarreraComponent},
+  {path:'altaperiodo', component:AltaperiodoComponent},
   {path:'home',component:DashboardComponent}
   
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HttpClientModule }    from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
 import {RegisterComponent} from './register/register.component';
 import { AltacarreraComponent } from './altacarrera/altacarrera.component';
+import { AltaperiodoComponent } from './altaperiodo/altaperiodo.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,6 +22,7 @@ import { AltacarreraComponent } from './altacarrera/altacarrera.component';
     LoginComponent,
     RegisterComponent,
     AltacarreraComponent,
+    AltaperiodoComponent,
 
   
   ],
