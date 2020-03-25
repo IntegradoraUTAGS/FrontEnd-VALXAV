05c0bf85d0bcd497d374c48b3fc24c0d6f725767
AdanJarami <est2615adan@gmail.com>
2020-03-25 13:10:43 -0600

    front cordinador

---
+++ src/app/pages/altacordinador/altacordinador.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-altacordinador',
+  templateUrl: './altacordinador.component.html',
+  styleUrls: ['./altacordinador.component.css']
+})
+export class AltacordinadorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
