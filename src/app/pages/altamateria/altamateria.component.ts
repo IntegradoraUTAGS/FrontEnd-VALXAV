1e76acf9ccfcc446c8bde79151b50b174d26ccaa
AdanJarami <est2615adan@gmail.com>
2020-03-25 11:42:12 -0600

    front materias

---
+++ src/app/pages/altamateria/altamateria.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-altamateria',
+  templateUrl: './altamateria.component.html',
+  styleUrls: ['./altamateria.component.css']
+})
+export class AltamateriaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
