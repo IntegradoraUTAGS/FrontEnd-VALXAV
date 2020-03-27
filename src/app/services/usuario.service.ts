2bb871c247594679341fe1dbed93605d4fc71e13
DanteGaytan <osvaldoyalllo@gmail.com>
2020-03-26 23:15:43 -0600

    Integracion terminada

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -7,39 +7,29 @@ import { Coordinador } from '../models/coordinador.model';
 })
 export class UsuarioService {
   url='http://localhost:3000/api'
-
   constructor(private http: HttpClient) { }
-
-
   registrar(usuario: Usuario){
     return this.http.post(`${this.url}/usuario/registrar`, usuario).toPromise();
   }
-
   login(usuario: Usuario){
     return this.http.post(`${this.url}/login`, usuario).toPromise();
   }
-
   getAcademia() {
     return this.http.get(`${this.url}/academia`);
   }
-
-
   postAcademia(nombre:any){
     return this.http.post(`${this.url}/academia`,{
       nombre:nombre
     });
   }
-
   deleteAcademia(id:any){
     return this.http.delete(`${this.url}/academia/${id}`)
   }
-
   putAcademia(id:any,nombre:any){
     return this.http.put(`${this.url}/academia/${id}`,{
       nombre:nombre
     });
   }
-
   postCoordinador(coordinador: Coordinador){
     return this.http.post(`${this.url}/coordinador/registrar`, coordinador).toPromise();
   }
