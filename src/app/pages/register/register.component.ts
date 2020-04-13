e54e6ec64784d7d446e27f71b407e4a26c671721
Luis Gerardo Castañeda López <55543994+REMKA-HAZE@users.noreply.github.com>
2020-04-13 16:43:45 -0500

    quitamos clgs

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -21,7 +21,7 @@ export class RegisterComponent implements OnInit {
     if (this.usuario.password == this.usuario.confirmPass) {
 
     this.servicio.registrar(this.usuario).then(resp => {
-      console.log(resp);
+     
       Swal.fire(
         'Good job!',
         'registrado con exito!',
@@ -30,7 +30,7 @@ export class RegisterComponent implements OnInit {
       this.router.navigateByUrl('login');
     }).catch((err: any) => {
       
-      console.log(err);
+      
       
       Swal.fire({
         icon: 'error',
