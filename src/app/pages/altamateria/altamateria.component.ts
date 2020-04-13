2af4e68f63a919625cfc768267e637a009737005
DanteGaytan <osvaldoyalllo@gmail.com>
2020-04-13 00:56:25 -0600

    ahora si ya jala

--- src/app/pages/altamateria/altamateria.component.ts
+++ src/app/pages/altamateria/altamateria.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Materia } from '../../models/materia.model';
 import { MateriaService } from 'src/app/services/materia.service';
-import { Acade } from 'src/app/models/acade.model';
+
 
 @Component({
   selector: 'app-altamateria',
@@ -10,27 +10,18 @@ import { Acade } from 'src/app/models/acade.model';
 })
 export class AltamateriaComponent implements OnInit {
 materia:Materia= new Materia();
-constructor(private servicio: MateriaService) { }
-acade: Acade = new Acade();
-acades: Acade[];
+constructor(private materiaService: MateriaService) { }
+
 
   ngOnInit() {
-   this.obtenerAcade();
+  
   }
   registrarmateria(){
-    this.servicio.postMateria(this.materia).then(resp =>{
-      console.log(resp);
-      //location.href
-    }).catch((err:any)=>{
-      console.log(err);
-    })
+    console.log(this.materia);
+    this.materiaService.registrarmateria(this.materia).then((respuesta) => {
+    }).catch((err) => {
+    });
     }
    
-  obtenerAcade(){
-      this.servicio.getAcade().then((resp: any) =>{
-      this.acades = resp.academia;
-        console.log(resp);
-      }).catch(()=>{
-      });
-   }
+  
 }
