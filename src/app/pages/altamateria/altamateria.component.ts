aca4e1ed15a2e4d1aac172cc749e9be610fc6bcd
DanteGaytan <osvaldoyalllo@gmail.com>
2020-04-17 16:44:16 -0500

    detalles materia

--- src/app/pages/altamateria/altamateria.component.ts
+++ src/app/pages/altamateria/altamateria.component.ts
@@ -1,7 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Materia } from '../../models/materia.model';
 import { MateriaService } from 'src/app/services/materia.service';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000
+ });
 
 @Component({
   selector: 'app-altamateria',
@@ -9,25 +18,24 @@ import { MateriaService } from 'src/app/services/materia.service';
   styleUrls: ['./altamateria.component.css']
 })
 export class AltamateriaComponent implements OnInit {
+ 
 materia:Materia= new Materia();
 
-constructor(private materiaService: MateriaService) { }
+constructor(public router: Router, private materiaService: MateriaService) { }
 
-
-acade: Acade = new Acade();
-acades: Acade[];
-  ngOnInit() {
 
 
   ngOnInit() {
-  
   }
-  registrarmateria(){
-    console.log(this.materia);
-    this.materiaService.registrarmateria(this.materia).then((respuesta) => {
-    }).catch((err) => {
+  
+  registrarmateria(forma:NgForm){
+    this.materiaService.registrarmateria(this.materia).then((materia: any) => {
+      Toast.fire(materia.msg, '', 'success');
+      forma.reset();
+      this.router.navigateByUrl('inicio');
+    }).catch((err: any) => {
+      Toast.fire(err.error.msg, '', 'error');
     });
-    }
-   
+  }
   
 }
